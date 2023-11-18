2ac3a87ce813182bae0c32b3129037e71b6716bf
Anzelm <marekd.90@gmail.com>
2023-11-18 20:44:53 +0100

    Timer chalenges

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Player from './components/Player.jsx';
+import TimerChalenge from "./components/TimerChalenge";
 
 function App() {
   return (
     <>
       <Player />
       <div id="challenges"></div>
+        <TimerChalenge targetTime={1} title={'Easy'}/>
+        <TimerChalenge targetTime={5} title={'Medium'}/>
+        <TimerChalenge targetTime={10} title={'Hard'}/>
+        <TimerChalenge targetTime={20} title={'Very hard'}/>
     </>
   );
 }
