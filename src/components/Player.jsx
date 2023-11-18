cc5b4fb7671f1a00dce5f9e9de577bb8f11935c8
Anzelm <marekd.90@gmail.com>
2023-11-18 20:42:17 +0100

    submit player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,22 @@
+import { useState, useRef } from 'react';
+
 export default function Player() {
+    const playerName = useRef();
+    const [submittedName, setSubmittedName] = useState(null);
+
+    // function handleChange (event) {
+    //     setSubmitted(false);
+    //     setSubmittedName(event.target.value);
+    // }
+    function submit(){
+        setSubmittedName(playerName.current.value);
+    }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submittedName ?? 'unknown'} entity</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text"/>
+        <button onClick={submit}>Set Name</button>
       </p>
     </section>
   );
