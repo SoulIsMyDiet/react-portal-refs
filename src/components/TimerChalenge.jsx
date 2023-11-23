70dc52bf345ee7693bf313bbdf217934237d07b7
Anzelm <marekd.90@gmail.com>
2023-11-23 14:37:16 +0100

    Timer chalenge mechanic; change name in docker-compose

--- src/components/TimerChalenge.jsx
+++ src/components/TimerChalenge.jsx
@@ -1,19 +1,36 @@
+import {useState, useRef} from 'react';
 export default function TimerChalenge({title, targetTime}){
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timerExpired, setTimerExpired] = useState(false);
+
+    let timer = useRef();
+    function handleStart(){
+        timer.current = setTimeout(() =>{
+            setTimerExpired(true);
+        }, targetTime * 1000);
+
+        setTimerStarted(true);
+    }
+
+    function handleStop(){
+        clearTimeout(timer.current);
+    }
     return (
         <section className="challenge">
             <h2>
                 {title}
             </h2>
+            {timerExpired && <p>You lost</p>}
             <p className="challenge-time">
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button>
-                    Start Challenge
+                <button onClick={timerStarted ? handleStop : handleStart}>
+                    {timerStarted ? 'Stop timer' :'Start Challenge'}
                 </button>
             </p>
-            <p className="">
-                Time is running / Timer in
+            <p className={timerStarted ? 'active' : undefined}>
+                {timerStarted ? 'Time is running' : 'Timer in'}
             </p>
         </section>
     )
